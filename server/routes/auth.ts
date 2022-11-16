7019f2423b4f8b38241a9b70d93d9334abdf9478
vongst <st@vong.io>
2022-11-16 15:19:15 +0800

    prettier formatting

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -2,37 +2,40 @@ const express = require("express");
 const authRoutes = express.Router();
 const dbo = require("../db/conn");
 const ObjectId = require("mongodb").ObjectId;
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
-const crypto = require('crypto');
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+const crypto = require("crypto");
 
 dotenv.config();
 process.env.TOKEN_SECRET;
 
 authRoutes.route("/auth").post(function (req, res) {
+  console.log("authRoutes /auth POST " + JSON.stringify(req.body));
 
-    console.log('authRoutes /auth POST ' + JSON.stringify(req.body))
+  const submitted_hash = crypto
+    .createHmac("sha256", process.env.SHA256_SALT)
+    .update(req.body.password)
+    .digest("hex");
 
-    const submitted_hash = crypto.createHmac("sha256", process.env.SHA256_SALT).update(req.body.password).digest('hex');
+  let db_connect = dbo.getDb();
+  db_connect
+    .collection("admin_users")
+    .findOne({ username: req.body.username }, function (err, result) {
+      if (err) {
+        throw err;
+      } else if (result.hash == submitted_hash) {
+        const token = jwt.sign(
+          {
+            username: req.body.username,
+          },
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+          { expiresIn: "1800s" }
+        );
+        res.status(200).json({ token: token, username: req.body.username });
+      } else {
+        res.status(401).json({ error: "no user" });
+      }
+    });
+});
 
-    let db_connect = dbo.getDb();
-    db_connect
-        .collection("admin_users")
-        .findOne({ username: req.body.username }, function (err, result) {
-        if (err) {
-            throw err
-        }
-        else if (result.hash == submitted_hash ) { 
-            const token = jwt.sign({
-                username: req.body.username}, 
-                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c", 
-                { expiresIn: '1800s' }
-            );
-            res.status(200).json({token: token, username: req.body.username}); 
-        } else {
-            res.status(401).json({error: 'no user'}) 
-        }
-        });
-  });
-  
 module.exports = authRoutes;
--- server/routes/product.ts
+++ server/routes/product.ts
@@ -11,14 +11,13 @@ const dbo = require("../db/conn");
 // This help convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
 
-
 // This section will help you get a list of all the products.
 productRoutes.route("/product").get(function (req, res) {
   let db_connect = dbo.getDb("mightyfoo");
   db_connect
     .collection("products")
     .find({})
-    .sort({ "_id" : -1})
+    .sort({ _id: -1 })
     .toArray(function (err, result) {
       if (err) throw err;
       res.json(result);
@@ -28,13 +27,11 @@ productRoutes.route("/product").get(function (req, res) {
 // This section will help you get a single record by id
 productRoutes.route("/product/:id").get(function (req, res) {
   let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId( req.params.id )};
-  db_connect
-      .collection("products")
-      .findOne(myquery, function (err, result) {
-        if (err) throw err;
-        res.json(result);
-      });
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect.collection("products").findOne(myquery, function (err, result) {
+    if (err) throw err;
+    res.json(result);
+  });
 });
 
 // This section will help you create a new record.
@@ -54,7 +51,7 @@ productRoutes.route("/product/add").post(function (req, response) {
 // This section will help you update a record by id.
 productRoutes.route("/update/:id").post(function (req, response) {
   let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId( req.params.id )};
+  let myquery = { _id: ObjectId(req.params.id) };
   let newvalues = {
     $set: {
       sku: req.body.sku,
@@ -74,7 +71,7 @@ productRoutes.route("/update/:id").post(function (req, response) {
 // This section will help you delete a record
 productRoutes.route("/:id").delete((req, response) => {
   let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId( req.params.id )};
+  let myquery = { _id: ObjectId(req.params.id) };
   db_connect.collection("products").deleteOne(myquery, function (err, obj) {
     if (err) throw err;
     console.log("1 document deleted");
