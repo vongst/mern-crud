909ed995c0b6534097191326b4ebd56ae46bb92c
vongst <st@vong.io>
2022-11-16 14:06:29 +0800

    cleanup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Button, Container, Form, Nav, Navbar } from 'react-bootstrap';
+import { Login } from './features/login/Login'
+import ProductList from './features/productlist/ProductList'
+import { useAppDispatch, useAppSelector } from './app/hooks'
+import { selectUserName, selectIsLoggedIn, userLogOut } from './features/login/loginSlice'
 
-import { Login } from './features/login/Login';
-import ProductList from './features/productlist/ProductList';
-import { useAppDispatch, useAppSelector } from './app/hooks';
-import { selectUserName, selectIsLoggedIn, userLogOut } from './features/login/loginSlice';
+import { Container, Nav, Navbar } from 'react-bootstrap'
 import { BsBoxArrowRight, BsPersonCheck } from 'react-icons/bs'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
@@ -19,19 +17,9 @@ function App() {
 
   return (
     <div>
-      <Navbar expand="lg" bg="light" variant="light">
+      <Navbar expand="lg" bg="dark" variant="dark" className='p-4'>
         <Container>
           <Navbar.Brand>MightyFoo Admin</Navbar.Brand>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search products"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-secondary">Search</Button>
-          </Form>
-
           {isLoggedIn ?
           <Nav className="justify-content-end">
             <Navbar.Text className="me-4">
@@ -46,7 +34,6 @@ function App() {
       
       <Container className="p-2">
        {isLoggedIn ? <ProductList /> : <Login /> }
-    
       </Container>
     </div>
   );
