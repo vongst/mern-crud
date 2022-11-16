6f92cc70569abf98ef7fa240713a7ca674a39230
vongst <st@vong.io>
2022-11-16 15:21:00 +0800

    removed API files

--- client/src/features/productlist/productListSlice.ts
+++ client/src/features/productlist/productListSlice.ts
@@ -1,6 +1,5 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState, AppThunk } from "../../app/store";
-import { fetchCount } from "./productListAPI";
 import axios from "axios";
 
 // export interface MyInterface extends Array<MyType> { }
